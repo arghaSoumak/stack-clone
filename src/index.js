8a04c758b0b645ba556b65a6d8f878a4f2945018
arghaSoumak <argha.soumak@gmail.com>
2018-11-10 19:16:37 +0530

    Modify the file Structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// css
+import './index.css';
+//app component
+import App from './App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+//render app in dom  
+const root =  document.getElementById('root')
+ReactDOM.render(<App />, root);
+
+/*for pwa*/
 serviceWorker.unregister();
